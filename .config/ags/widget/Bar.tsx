81b6623ecccbf17b229359331e6d463b9b5bb2d0
Falconbird18 <austintallent25@outlook.com>
2024-11-22 22:54:09 -0800

    Fix battery so that it updates dynamically.

--- .config/ags/widget/Bar.tsx
+++ .config/ags/widget/Bar.tsx
@@ -1,31 +1,40 @@
-import { App, Astal, Gtk, Gdk } from "astal/gtk3"
-import { Variable } from "astal"
-import Battery from "gi://AstalBattery"
+import { App, Astal, Gtk, Gdk } from "astal/gtk3";
+import { Variable } from "astal";
+import Battery from "gi://AstalBattery";
 
-const battery = Battery.get_default()
-const time = Variable("").poll(1000, "date")
+const battery = Battery.get_default();
+const time = Variable("").poll(1000, "date");
+
+// Create a variable that polls the battery percentage every 5 seconds
+const batteryPercentage = Variable("").poll(5000, () =>
+    `${(battery.percentage * 100).toFixed(0)}%`
+);
 
 export default function Bar(gdkmonitor: Gdk.Monitor) {
-    return <window
-        className="Bar"
-        gdkmonitor={gdkmonitor}
-        exclusivity={Astal.Exclusivity.EXCLUSIVE}
-        anchor={Astal.WindowAnchor.TOP
-            | Astal.WindowAnchor.LEFT
-            | Astal.WindowAnchor.RIGHT}
-        application={App}>
-        <centerbox>
-            <button
-                onClick={() => print("hello")}
-                halign={Gtk.Align.START} >
-                <label label={time()} />
-            </button>
-            <button
-                onClick={() => print("hello")}
-                halign={Gtk.Align.END} >
-                <label label={`${(battery.percentage * 100).toFixed(0)}%`} />
-            </button>
-            <box />
-        </centerbox>
-    </window>
+    return (
+        <window
+            className="Bar"
+            gdkmonitor={gdkmonitor}
+            exclusivity={Astal.Exclusivity.EXCLUSIVE}
+            anchor={
+                Astal.WindowAnchor.TOP |
+                Astal.WindowAnchor.LEFT |
+                Astal.WindowAnchor.RIGHT
+            }
+            application={App}>
+            <centerbox>
+                <button
+                    onClick={() => print("hello")}
+                    halign={Gtk.Align.START}>
+                    <label label={time()} />
+                </button>
+                <button
+                    onClick={() => print("hello")}
+                    halign={Gtk.Align.END}>
+                    <label label={batteryPercentage()} />
+                </button>
+                <box />
+            </centerbox>
+        </window>
+    );
 }
