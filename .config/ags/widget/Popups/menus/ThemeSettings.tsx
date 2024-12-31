a2e2e00e65b6ec725a9e322c3f476aba384e2517
Falconbird18 <austintallent25@outlook.com>
2024-12-30 23:20:36 -0800

    Work on theme settings.

--- .config/ags/widget/Popups/menus/ThemeSettings.tsx
+++ .config/ags/widget/Popups/menus/ThemeSettings.tsx
@@ -6,32 +6,6 @@ import { feelsTemp, humidity, location, precipitation, pressure, realTemp, uvInd
 import icons from "../../../lib/icons";
 import PopupMenu from "../PopupMenu";
 
-const temperature = bind(realTemp);
-const feelsTemperature = bind(feelsTemp);
-const uv = bind(uvIndex);
-const Wind = bind(wind);
-const Precipitation = bind(precipitation);
-const Pressure = bind(pressure);
-const Humidity = bind(humidity);
-
-const displayLocation = location.replace(",", ", ")
-const icon = icons.ui.edit
-
-const Entry = new Widget.Entry({
-    text: "Test",
-    canFocus: true,
-    className: "location_input",
-    // onActivate: () => {
-    //     items.get()[0]?.app.launch();
-    //     App.toggle_window("app-launcher");
-    // },
-    // setup: (self) => {
-    //     self.hook(self, "notify::text", () => {
-    //         query.set(self.get_text());
-    //     });
-    // },
-});
-
 export default () => {
     return (
         <PopupMenu
@@ -40,67 +14,17 @@ export default () => {
             namespace="theme-settings"
             label="theme-settings"
         >
-            <box
-                vertical
-                className="theme-settings"
-                spacing={spacing}
-            >
-                <box
-                    horizontal
-                    className="location-header-container"
+            <box className="theme-buttons-container">
+                <button
+                    className="theme-settings__button"
                 >
-                    <label
-                        label={displayLocation}
-                        className="location"
-                        halign={Gtk.Align.START}
-                    />
-                    <box
-                        horizontal
-                        halign={Gtk.Align.CENTER}
-                    >
-                        <button
-                            className="icon-button"
-                            valign={Gtk.Align.CENTER}
-                        >
-                            <icon
-                                icon={icon}
-                            />
-                        </button>
-                    </box>
-                </box>
-                {Entry}
-                <label
-                    label={temperature}
-                    className="temperature"
-                    halign={Gtk.Align.START}
-
-                />
-                <box horizontal className="weather-info" spacing={spacing}>
-                    <box vertical className="weather-info-title">
-                        <label label="Humidity" />
-                        <label label={Humidity} />
-                    </box>
-                    <box vertical className="weather-info-title">
-                        <label label="Wind" />
-                        <label label={Wind} />
-                    </box>
-                    <box vertical className="weather-info-title">
-                        <label label="Precipitation" />
-                        <label label={Precipitation} />
-                    </box>
-                    <box vertical className="weather-info-title">
-                        <label label="Pressure" />
-                        <label label={Pressure} />
-                    </box>
-                    <box vertical className="weather-info-title">
-                        <label label="UV Index" />
-                        <label label={uv} />
-                    </box>
-                    <box vertical className="weather-info-title">
-                        <label label="Feels like" />
-                        <label label={feelsTemperature} />
-                    </box>
-                </box>
+                    <label label="Light" />
+                </button>
+                <button
+                    className="theme-settings__button"
+                >
+                    <label label="Dark" />
+                </button>
             </box>
         </PopupMenu>
     );
