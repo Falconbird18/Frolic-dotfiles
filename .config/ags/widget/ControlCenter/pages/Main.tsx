e3c03fa8491352c90e3511e0e14c9549b4a9b926
falconbird18 <austintallent25@outlook.com>
2025-02-20 15:48:01 -0800

    Finish capture container

--- .config/ags/widget/ControlCenter/pages/Main.tsx
+++ .config/ags/widget/ControlCenter/pages/Main.tsx
@@ -28,11 +28,21 @@ class FlowBox extends astalify(Gtk.FlowBox) {
   }
 }
 
+class Box extends astalify(Gtk.Box) {
+  static {
+    GObject.registerClass(this);
+  }
+
+  constructor(props: ConstructProps<Gtk.Box, Gtk.Box.ConstructorProps>) {
+    super(props as any);
+  }
+}
+
 export default () => {
   const revealScreenRecord = Variable(false);
 
   const fb = new FlowBox({
-    homogeneous: true,
+    homogeneous: true, // You can keep it true or false, depending on your desired FlowBox behavior
     selectionMode: Gtk.SelectionMode.NONE,
     maxChildrenPerLine: 2,
     minChildrenPerLine: 2,
@@ -43,96 +53,88 @@ export default () => {
   const FanProfile = FanProfileButton();
   const Network = NetworkButton();
   const Bluetooth = BluetoothButton();
-  <button
-  className="control-center__settings-button"
-  onClick={() => toggleWindow("popup-theme-settings")}
-  >
-  <icon icon={icons.ui.settings} iconSize={16} />
-  </button>
 
-  const settingsContainer = new Widget.Box({
+  const connectivityContainer = new Widget.Box({
     spacing,
     className: "control-center__settings-container",
     vertical: true,
     children: [
-      new Widget.Label({ label: "Connectivity",
-        className: "h1",
-      }),
+      new Widget.Label({ label: "Connectivity", className: "h1" }),
       Network ? Network : null,
       Bluetooth ? Bluetooth : null,
-    ].filter(Boolean), // Filter out any null values if any of Network, Bluetooth, FanProfile are not valid widgets
+    ].filter(Boolean),
   });
 
+  const Container = new Widget.Box({
+    spacing,
+    className: "control-center__settings-container",
+    vertical: true,
+    children: [DND(), FanProfile].filter(Boolean),
+  });
 
-  fb.add(settingsContainer);
-  if (FanProfile != undefined) {
-    fb.add(FanProfile);
-  }
-  fb.add(Microphone());
-  fb.add(DND());
-  fb.add(
-    new Widget.Box({
-      spacing,
-      homogeneous: true,
-      children: [
-        ScreenRecord({
-          onClicked: () => {
-            if (ScreenRecordService.recording) {
-              ScreenRecordService.stop();
-            } else {
-              revealScreenRecord.set(!revealScreenRecord.get());
-            }
-          },
-        }),
-      ],
-    }),
-  );
-  fb.add(
-    new Widget.Box({
-      spacing,
-      homogeneous: true,
-      children: [
-        ScreenShot(),
-                   ScreenSnip(),
-      ],
-    }),
-  );
+  const recordingContainer = new Widget.Box({
+    spacing,
+    className: "control-center__wide-settings-container",
+    vertical: true,
+    children: [
+      new Widget.Label({ label: "Capture", className: "h1" }),
+      new Widget.Box({
+        vertical: false,
+        children: [
+          Microphone(),
+          ScreenShot(),
+          ScreenSnip(),
+          ScreenRecord({
+            onClicked: () => {
+              if (ScreenRecordService.recording) {
+                ScreenRecordService.stop();
+              } else {
+                revealScreenRecord.set(!revealScreenRecord.get());
+              }
+            },
+          }),
+        ],
+      }),
+    ].filter(Boolean),
+  });
+
+  fb.add(connectivityContainer);
+  fb.add(Container);
 
   return (
     <box
-    name="main"
-    className="control-center__page main"
-    vertical
-    spacing={spacing}
+      name="main"
+      className="control-center__page main"
+      vertical
+      spacing={spacing}
     >
-    {fb}
-    <ScreenRecordMenu
-    revealMenu={bind(revealScreenRecord)}
-    closeMenu={() => revealScreenRecord.set(!revealScreenRecord.get())}
-    />
-    <box
-    horizontal
-    spacing={spacing}
-    >
-    <Volume />
-    {Brightness()}
-    </box>
-    <box spacing={16} className="control-center__footer">
-    <button
-    className="control-center__powermenu-button"
-    onClick={() => toggleWindow("powermenu")}
-    >
-    <icon icon={icons.powermenu.shutdown} iconSize={16} />
-    </button>
-    <box hexpand />
-    <label className="control-center__time-to-empty" label={bind(uptime)} />
-    <button
-    className="control-center__settings-button"
-    onClick={() => toggleWindow("popup-theme-settings")}
-    >
-    <icon icon={icons.ui.settings} iconSize={16} />
-    </button>
-    </box>
+      {fb} {/* FlowBox remains in the vertical box */}
+      {recordingContainer}{" "}
+      {/* recordingContainer is now a direct child of the vertical box */}
+      <ScreenRecordMenu
+        revealMenu={bind(revealScreenRecord)}
+        closeMenu={() => revealScreenRecord.set(!revealScreenRecord.get())}
+      />
+      <box horizontal spacing={spacing}>
+        <Volume />
+        {Brightness()}
+      </box>
+      <box spacing={16} className="control-center__footer">
+        <button
+          className="control-center__powermenu-button"
+          onClick={() => toggleWindow("powermenu")}
+        >
+          <icon icon={icons.powermenu.shutdown} iconSize={16} />
+        </button>
+        <box hexpand />
+        <label className="control-center__time-to-empty" label={bind(uptime)} />
+        <button
+          className="control-center__settings-button"
+          onClick={() => toggleWindow("popup-theme-settings")}
+        >
+          <icon icon={icons.ui.settings} iconSize={16} />
+        </button>
+      </box>
     </box>
   );
 };
